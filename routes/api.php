87837397e12caa4e50812d8b58258ced33375932
Justin DeRidder <j.deridder@arcane.ws>
2017-11-01 16:17:26 -0400

    API to manage user groups to discount code mapping

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,36 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'group'], function() {
+    Route::get('/all', function (Request $request) {
+        return App\UserGroup::all();
+    });
+
+    Route::get('/{id}', function ($id) {
+        return App\UserGroup::findOrFail($id);
+    });
+
+    Route::get('/', function (Request $request) {
+        return App\UserGroup::where('group_name', $request->input('name'))->firstOrFail();
+    });
+
+    Route::post('/', function (Request $request) {
+        App\UserGroup::create(['group_name' => $request->input('group_name'), 'discount_id' => intval($request->input('discount_id'))]);
+    });
+
+    Route::put('/{id}', function ($id, Request $request) {
+        $group = App\UserGroup::findOrFail($id);
+
+        $group->discount_id = intval($request->input('discount_id'));
+
+        $group->save();
+    });
+
+    Route::delete('/{id}', function ($id, Request $request) {
+        $group = App\UserGroup::findOrFail($id);
+
+        $group->delete();
+    });
 });
+
+
