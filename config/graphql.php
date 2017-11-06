0da2cefeb10eec91f7e835aa704c26c268e84100
Tom Eastwood <tomeastwood@Toms-MacBook-Pro.local>
2017-11-06 15:20:37 -0500

    forgot to add graphql config

--- config/graphql.php
+++ config/graphql.php
@@ -84,13 +84,8 @@ return [
         'default' => [
             'query' => [
                 'countries' => 'App\GraphQL\Query\CountryQuery',
-//                'regions' => 'App\GraphQL\Query\RegionQuery',
-//                'cities' => 'App\GraphQL\Query\CityQuery',
-//                'locations' => 'App\GraphQL\Query\LocationQuery',
             ],
             'mutation' => [
-//                'addLocationMutation' => 'App\GraphQL\Mutation\AddLocationMutation',
-//                'updateLocationMutation' => 'App\GraphQL\Mutation\UpdateLocationMutation'
             ]
         ],
     ],
@@ -106,12 +101,6 @@ return [
     //
     'types' => [
         'Country' => 'App\GraphQL\Type\CountryType',
-        //'Region' => 'App\GraphQL\Type\RegionType',
-//        'City' => 'App\GraphQL\Type\CityType',
-//        'Location' => 'App\GraphQL\Type\LocationType',
-//        'Timezone' => 'App\GraphQL\Type\TimezoneType',
-//        'Address' => 'App\GraphQL\Type\AddressType',
-//        'AddressType' => 'App\GraphQL\Type\AddressTypeType'
     ],
     
     // This callable will be passed the Error object for each errors GraphQL catch.
