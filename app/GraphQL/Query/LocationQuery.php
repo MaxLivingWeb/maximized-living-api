64dd311ddae60c0e877d95d983f4d7b4792a51c9
Tom Eastwood <tomeastwood@Toms-MacBook-Pro.local>
2017-11-06 17:01:37 -0500

    working through logic to filter by radius

--- app/GraphQL/Query/LocationQuery.php
+++ app/GraphQL/Query/LocationQuery.php
@@ -31,12 +31,40 @@ class LocationQuery extends Query
             'slug' => [
                 'name' => 'slug',
                 'type' => Type::string()
+            ],
+            'filter_by_radius' => [
+                'name' => 'filter_by_radius',
+                'type' => Type::boolean(),
+                'description' => 'boolean if you want to filter location by radius'
+            ],
+            'latitude' => [
+                'type' => Type::float(),
+                'description' => 'latitude'
+            ],
+            'longitude' => [
+                'type' => Type::float(),
+                'description' => 'longitude'
+            ],
+            'distance' => [
+                'type' => Type::int(),
+                'description' => 'the radius of your query'
             ]
         ];
     }
 
     public function resolve ($root, $args)
     {
+        //we have the parameters need for a filter by radius
+        if($args['filter_by_radius'] === TRUE && isset($args['latitude']) && isset($args['longitude']) && isset($args['distance'])) {
+
+            $lat = $args['latitude'];
+            $long = $args['longitude'];
+            $distance = $args['distance'];
+            $filtered_locations = DB::select("SELECT * FROM Locations WHERE acos(sin(1.3963) * sin($lat) + cos(1.3963) * cos($lat) * cos($long - (-0.6981))) * 6371 <= $distance");
+
+            return $filtered_locations;
+        }
+
         if (isset($args['id'])) {
             return Location::where('id', $args['id'])->get();
         }
