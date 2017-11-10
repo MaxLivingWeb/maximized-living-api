e078296c7713e50ab40b274c1856d2f05b2df6bf
Alex Jones-Chick <a.joneschick@arcane.ws>
2017-11-10 14:12:26 -0500

    Updated formatting, added sanitization, and general cleaning of code.

--- app/GraphQL/Mutation/AddLocationMutation.php
+++ app/GraphQL/Mutation/AddLocationMutation.php
@@ -22,92 +22,132 @@ class AddLocationMutation extends Mutation
     {
         //these are the parameters that are need - some can be null - but the parameter itself needs to be in the mutation
         return [
-            'name' => ['name' => 'name', 'type' => Type::nonNull(Type::string())],
-            'zip_postal_code' => ['name' => 'zip_postal_code', 'type' => Type::nonNull(Type::string())],
-            'latitude' => ['name' => 'latitude', 'type' => Type::nonNull(Type::float())],
-            'longitude' => ['name' => 'longitude', 'type' => Type::nonNull(Type::float())],
-            'telephone' => ['name' => 'telephone', 'type' => Type::nonNull(Type::string())],
-            'telephone_ext' => ['name' => 'telephone_ext', 'type' => Type::string()],
-            'fax' => ['name' => 'fax', 'type' => Type::string()],
-            'email' => ['name' => 'email', 'type' => Type::nonNull(Type::string())],
-            'vanity_website_url' => ['name' => 'vanity_website_url', 'type' => Type::string()],
-            'pre_open_display_date' => ['name' => 'pre_open_display_date', 'type' => Type::string()],
-            'opening_date' => ['name' => 'opening_date', 'type' => Type::string()],
-            'closing_date' => ['name' => 'closing_date', 'type' => Type::string()],
-            'daylight_savings_applies' => ['name' => 'daylight_savings_applies', 'type' => Type::boolean()],
-            'timezone_id' => ['name' => 'timezone_id', 'type' => Type::nonNull(Type::int())],
+            'id' => [
+                'name' => 'id',
+                'type' => Type::nonNull(Type::int())
+            ],
+            'name' => [
+                'name' => 'name',
+                'type' => Type::nonNull(Type::string())
+            ],
+            'zip_postal_code' => [
+                'name' => 'zip_postal_code',
+                'type' => Type::nonNull(Type::string())
+            ],
+            'latitude' => [
+                'name' => 'latitude',
+                'type' => Type::nonNull(Type::float())
+            ],
+            'longitude' => [
+                'name' => 'longitude',
+                'type' => Type::nonNull(Type::float())
+            ],
+            'telephone' => [
+                'name' => 'telephone',
+                'type' => Type::nonNull(Type::string())
+            ],
+            'telephone_ext' => [
+                'name' => 'telephone_ext',
+                'type' => Type::string()],
+            'fax' => [
+                'name' => 'fax',
+                'type' => Type::string()
+            ],
+            'email' => [
+                'name' => 'email',
+                'type' => Type::nonNull(Type::string())
+            ],
+            'vanity_website_url' => [
+                'name' => 'vanity_website_url',
+                'type' => Type::string()
+            ],
+            'pre_open_display_date' => [
+                'name' => 'pre_open_display_date',
+                'type' => Type::string()
+            ],
+            'opening_date' => [
+                'name' => 'opening_date',
+                'type' => Type::string()
+            ],
+            'closing_date' => [
+                'name' => 'closing_date',
+                'type' => Type::string()
+            ],
+            'daylight_savings_applies' => [
+                'name' => 'daylight_savings_applies',
+                'type' => Type::int()
+            ],
+            'timezone_id' => [
+                'name' => 'timezone_id',
+                'type' => Type::nonNull(Type::int())
+            ]
         ];
     }
-
+    
     public function resolve($root, $args)
     {
-        $location_slug = str_replace(' ', '-', strtolower($args['name']) );
-
-        if(Location::where("slug", $location_slug)->exists() ) {
-            return;
+        $location_slug = str_slug($args['name']);
+        
+        foreach ($args as $key => $var) {
+            $args[$key] = filter_var($var, FILTER_SANITIZE_STRING);
         }
-
-        $location = new Location();
-
-        //TODO figure out what is needed of an affiliate id and operating hours (hardcoded a JSON string for now)
-        $location->affiliate_id = "123";
-        $location->name = $args['name'];
-        $location->zip_postal_code = $args['zip_postal_code'];
-        $location->latitude = $args['latitude'];
-        $location->longitude = $args['longitude'];
-        $location->telephone = $args['telephone'];
-        $location->telephone_ext = $args['telephone_ext'];
-        $location->fax = $args['fax'];
-        $location->email = $args['email'];
-        $location->vanity_website_url = $args['vanity_website_url'];
-        $location->slug = $location_slug;
-        $location->pre_open_display_date = $args['pre_open_display_date'];
-        $location->opening_date = $args['opening_date'];
-        $location->closing_date = $args['closing_date'];
-        $location->daylight_savings_applies = $args['daylight_savings_applies'];
-        $location->operating_hours = "\"businessHours\": {
-            \"periods\": [
-            {
-              \"openDay\": \"MONDAY\",
-              \"closeDay\": \"MONDAY\",
-              \"openTime\": \"09:00\",
-              \"closeTime\": \"17:00\"
-            },
-            {
-              \"openDay\": \"TUESDAY\",
-              \"closeDay\": \"TUESDAY\",
-              \"openTime\": \"09:00\",
-              \"closeTime\": \"17:00\"
-            },
-            {
-              \"openDay\": \"TUESDAY\",
-              \"closeDay\": \"TUESDAY\",
-              \"openTime\": \"09:00\",
-              \"closeTime\": \"17:00\"
-            },
-            {
-              \"openDay\": \"WEDNESDAY\",
-              \"closeDay\": \"WEDNESDAY\",
-              \"openTime\": \"09:00\",
-              \"closeTime\": \"17:00\"
-            },
-            {
-              \"openDay\": \"THURSDAY\",
-              \"closeDay\": \"THURSDAY\",
-              \"openTime\": \"09:00\",
-              \"closeTime\": \"17:00\"
-            },
-            {
-              \"openDay\": \"FRIDAY\",
-              \"closeDay\": \"FRIDAY\",
-              \"openTime\": \"09:00\",
-              \"closeTime\": \"17:00\"
-            },
-          ]
-        },}"; //hardcoding for now - need widget
-        $location->timezone_id = $args['timezone_id'];
-
-        $location->save();
+        
+        $business_hours = [
+            'businessHours' => [
+                'periods' => [
+                    'openDay' => 'MONDAY',
+                    'closeDay' => 'MONDAY',
+                    'openTime' => '09:00',
+                    'closeTime' => '17:00'
+                ],
+                [
+                    'openDay' => 'TUESDAY',
+                    'closeDay' => 'TUESDAY',
+                    'openTime' => '09:00',
+                    'closeTime' => '17:00'
+                ],
+                [
+                    'openDay' => 'WEDNESDAY',
+                    'closeDay' => 'WEDNESDAY',
+                    'openTime' => '09:00',
+                    'closeTime' => '17:00'
+                ],
+                [
+                    'openDay' => 'THURSDAY',
+                    'closeDay' => 'THURSDAY',
+                    'openTime' => '09:00',
+                    'closeTime' => '17:00'
+                ],
+                [
+                    'openDay' => 'FRIDAY',
+                    'closeDay' => 'FRIDAY',
+                    'openTime' => '09:00',
+                    'closeTime' => '17:00'
+                ],
+            ]
+        ];
+        
+        $location = Location::create([
+            'affiliate_id'              => "123",
+            'name'                      => $args['name'],
+            'zip_postal_code'           => $args['zip_postal_code'],
+            'latitude'                  => $args['latitude'],
+            'longitude'                 => $args['longitude'],
+            'telephone'                 => $args['telephone'],
+            'telephone_ext'             => $args['telephone_ext'],
+            'fax'                       => $args['fax'],
+            'email'                     => $args['email'],
+            'vanity_website_url'        => $args['vanity_website_url'],
+            'slug'                      => $location_slug,
+            'pre_open_display_date'     => $args['pre_open_display_date'],
+            'opening_date'              => $args['opening_date'],
+            'closing_date'              => $args['closing_date'],
+            'daylight_savings_applies'  => $args['daylight_savings_applies'],
+            'operating_hours'           => json_encode($business_hours),
+            'timezone_id'               => $args['timezone_id']
+         ]);
+        
+        return $location;
     }
 
 }
