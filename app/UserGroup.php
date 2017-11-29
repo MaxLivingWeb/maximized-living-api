15f33a15cdcbf0f6bc74be3754eab9bdb4b459db
Justin DeRidder <j.deridder@arcane.ws>
2017-11-29 12:08:34 -0500

    Revert "-reordering migrations"
    This reverts commit 9a928eab21346da3ab13e66adf44c2e232447676.

--- app/UserGroup.php
+++ app/UserGroup.php
@@ -26,8 +26,4 @@ class UserGroup extends Model
     public function commission() {
         return $this->hasOne('App\CommissionGroup', 'id', 'commission_id');
     }
-
-    public function location() {
-        return $this->hasOne('App\Location', 'id', 'location_id');
-    }
 }
