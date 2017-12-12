52555b347e07f178a8930840cb7db8fdd3414f45
jamieson-roberts <j.roberts@arcane.ws>
2017-12-12 16:57:48 -0500

    Removing foreign key constraint

--- database/migrations/2017_12_12_220948_create_transactional_emails_table.php
+++ database/migrations/2017_12_12_220948_create_transactional_emails_table.php
@@ -20,15 +20,12 @@ class CreateTransactionalEmailsTable extends Migration
             $table->string('to_name')->nullable();
             $table->string('to_email');
             $table->string('email_subject');
-            $table->integer('vanity_website_id');
+            $table->integer('vanity_website_id')->unsigned();
             $table->string('form_name');
             $table->longText('request_data');
             $table->integer('leads_api_submission_status');
             $table->integer('sendgrid_submission_status');
-
             $table->timestamps();
-
-            $table->foreign('vanity_website_id')->references('vanity_website_id')->on('locations');
         });
     }
 
