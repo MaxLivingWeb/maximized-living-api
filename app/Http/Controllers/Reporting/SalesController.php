ec9b87a0f38b36d7e6de8ddfde2302ccd93c18fb
Justin DeRidder <j.deridder@arcane.ws>
2017-12-20 13:41:05 -0500

    endpoint to get all retail sales

---
+++ app/Http/Controllers/Reporting/SalesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ShopifyHelper;
+use Illuminate\Http\Request;
+
+class SalesController extends Controller
+{
+    public function sales(Request $request)
+    {
+        try {
+            $shopify = new ShopifyHelper();
+
+            $startDate = null;
+            $endDate = null;
+            if(request()->has('startDate') || request()->has('endDate')) {
+                $request->validate([
+                    'startDate' => 'required|date',
+                    'endDate'   => 'required|date'
+                ]);
+
+                $startDate = new \DateTime(request()->input('startDate'));
+                $endDate = new \DateTime(request()->input('endDate'));
+            }
+
+            $orders = $shopify->getAllOrders($startDate, $endDate);
+
+            return $orders->filter(function ($value) {
+                return !collect($value->note_attributes)->contains('name', 'wholesaleId');
+            })->values();
+        }
+        catch (\Exception $e) {
+            return response()->json($e->getMessage(), 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -50,6 +50,8 @@ Route::group(['prefix' => 'permissions'], function() {
 Route::post('/contact', 'TransactionalEmailController@save');
 
 Route::group(['prefix' => 'reporting'], function() {
+    Route::get('/sales', 'SalesController@sales');
+
     Route::group(['prefix' => 'affiliate'], function() {
         Route::get('/sales', 'AffiliateController@sales');
     });
