3939b512f403f2fee24bd1d04fde5c0f623f3e52
Jessica Mundt <j.mundt@arcane.ws>
2017-12-22 16:50:13 -0500

    Added 'getDateObject' helper method

--- app/Http/Controllers/Reporting/SalesController.php
+++ app/Http/Controllers/Reporting/SalesController.php
@@ -12,22 +12,37 @@ class SalesController extends Controller
         try {
             $shopify = new ShopifyHelper();
 
-            $startDate = null;
-            $endDate = null;
-            if(request()->has('startDate') || request()->has('endDate')) {
-                $request->validate([
-                    'startDate' => 'required|date',
-                    'endDate'   => 'required|date'
-                ]);
-
-                $startDate = new \DateTime(request()->input('startDate'));
-                $endDate = new \DateTime(request()->input('endDate'));
-            }
+            $dateObject = $this->getDateObject($request);
+            $startDate = $dateObject->startDate;
+            $endDate = $dateObject->endDate;
 
             return $shopify->getAllOrders($startDate, $endDate);
         }
         catch (\Exception $e) {
             return response()->json($e->getMessage(), 500);
         }
+    }
+
+    /**
+     * Get Start and End date from the current Request
+     */
+    private function getDateObject(Request $request)
+    {
+        $startDate = null;
+        $endDate = null;
+        if(request()->has('startDate') || request()->has('endDate')) {
+            $request->validate([
+                'startDate' => 'required|date',
+                'endDate'   => 'required|date'
+            ]);
+
+            $startDate = new \DateTime(request()->input('startDate'));
+            $endDate = new \DateTime(request()->input('endDate'));
+        }
+
+        return (Object)[
+            'startDate' => $startDate,
+            'endDate' => $endDate
+        ];
     }
 }
