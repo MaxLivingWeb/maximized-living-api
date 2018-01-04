f7514d25fcc1e0598d9ccaa01c89224bf16a41d5
Justin DeRidder <j.deridder@arcane.ws>
2018-01-04 13:55:07 -0500

    accept event promoter when creating a new affiliate/group

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -113,6 +113,11 @@ class GroupController extends Controller
                 $premium = boolval($request->input('premium'));
             }
 
+            $event_promoter = false;
+            if (!is_null($request->input('event_promoter'))) {
+                $event_promoter = boolval($request->input('event_promoter'));
+            }
+
             if($request->has('wholesale.shipping') && !is_null($location_id)) {
                 $shippingAddress = Address::create([
                     'address_1' => $request->input('wholesale.shipping.address_1'),
@@ -164,7 +169,8 @@ class GroupController extends Controller
                 'discount_id'        => $discount_id,
                 'commission_id'      => $commission_id,
                 'location_id'        => $location_id,
-                'premium'            => $premium
+                'premium'            => $premium,
+                'event_promoter'     => $event_promoter
             ]);
         }
         catch (ValidationException $e) {
