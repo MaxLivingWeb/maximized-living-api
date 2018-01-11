d919bde00befc25db2fd74d05ce60f6431a35c80
Adrian Verhagen <a.verhagen@arcane.ws>
2018-01-11 12:24:37 -0500

    Add user group stuff to radius query, also fix SQL injection vulnerability

--- app/GraphQL/Type/OutputLocationType.php
+++ app/GraphQL/Type/OutputLocationType.php
@@ -114,6 +114,21 @@ class OutputLocationType extends LocationType
             'type' => Type::string()
         ];
 
+        $fields['user_group_id'] = [
+            'name' => 'user_group_id',
+            'type' => Type::int()
+        ];
+
+        $fields['user_group_premium'] = [
+            'name' => 'user_group_premium',
+            'type' => Type::int()
+        ];
+
+        $fields['user_group_event_promoter'] = [
+            'name' => 'user_group_event_promoter',
+            'type' => Type::int()
+        ];
+
         $fields['user_group']  = [
             'args' => [
                 'id' => [
