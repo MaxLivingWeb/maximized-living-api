39427017964a8ecdb174069f064bc22623cdca0e
Christian Griffin <c.griffin@arcane.ws>
2018-02-05 15:22:03 -0500

    Added more detailed error reporting.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -66,18 +66,26 @@ class Handler extends ExceptionHandler
      */
     private function _notify(Exception $e): void
     {
-        if ($this->isHttpException($e) && $e->getStatusCode() !== 404) {
+        if ($this->isHttpException($e) /*&& $e->getStatusCode() !== 404*/) {
             if(config('app.env') == 'local' && empty(env('LOCAL_ERROR_SLACK_ID'))) {
                 return;
             }
-            $code = $e->getStatusCode() ?? $e->getCode() ?? '';
+            $code = $e->getStatusCode() ?? $e->getCode() ?? NULL;
             $message = $e->getMessage();
             $application = config('app.name');
             $environment = config('app.env');
             $trace = $e->getTraceAsString();
 
+            $request = request();
+            $url = $request->fullUrl() ?? NULL;
+            $method = $request->method() ?? NULL;
+            $referer = $request->server()['HTTP_REFERER'] ?? NULL;
+
             $message = '*Application*: ' . $application . "\n" .
                 '*Environment*: ' . $environment . "\n" .
+                (!empty($url) ? '*URL*: ' . $url . "\n" : '') .
+                (!empty($method) ? '*Method*: ' . $method . "\n" : '') .
+                (!empty($referer) ? '*Referer*: ' . $referer . "\n" : '') .
                 (!empty($code) ? '*Code*: ' . $code . "\n" : '') .
                 (!empty($message) ? '*Code*: ' . $message . "\n" : '') .
                 '*Trace*: ' . "\n" . $trace . "\n" .
