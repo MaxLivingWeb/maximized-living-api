40857fb040f6418fc76b75a35f141803d0e25653
Christian Griffin <c.griffin@arcane.ws>
2018-02-05 15:26:07 -0500

    Uncommented 404 condition.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -66,7 +66,7 @@ class Handler extends ExceptionHandler
      */
     private function _notify(Exception $e): void
     {
-        if ($this->isHttpException($e) /*&& $e->getStatusCode() !== 404*/) {
+        if ($this->isHttpException($e) && $e->getStatusCode() !== 404) {
             if(config('app.env') == 'local' && empty(env('LOCAL_ERROR_SLACK_ID'))) {
                 return;
             }
