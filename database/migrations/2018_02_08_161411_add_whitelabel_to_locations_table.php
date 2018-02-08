78180a65feed3d84b2199da6b12e30f04299422c
Liam Stewart <l.stewart@arcane.ws>
2018-02-08 11:18:07 -0500

    Adding column

---
+++ database/migrations/2018_02_08_161411_add_whitelabel_to_locations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWhitelabelToLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->boolean('whitelabel')->default(0)->after('vanity_website_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->dropColumn('whitelabel');
+        });
+    }
+}
