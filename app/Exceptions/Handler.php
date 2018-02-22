f77b459ba040742c6feb735798ab487ed2f08df4
Christian Griffin <c.griffin@arcane.ws>
2018-02-22 09:26:35 -0500

    Don't report errors on local.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,8 +38,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-        if (app()->bound('sentry') && $this->shouldReport($exception)) {
-            app('sentry')->captureException($exception);
+        if(config('app.env') !== 'local') {
+            if (app()->bound('sentry') && $this->shouldReport($exception)) {
+                app('sentry')->captureException($exception);
+            }
         }
 
         parent::report($exception);
