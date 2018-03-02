88f9125c25c0b6bc5caae664f9b7fcbcc787c96b
Jessica Mundt <j.mundt@arcane.ws>
2018-03-02 11:29:02 -0500

    Find all Cognito users that have uppercased emails

---
+++ app/Console/Commands/FindUppercasedCognitoUserEmails.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\CognitoUser;
+use Illuminate\Console\Command;
+use App\Helpers\CognitoUserHelper;
+
+class FindUppercasedCognitoUserEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:findUppercasedCognitoUserEmails';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fnd all Cognito user emails that have uppercase letters';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $response = $this->line('Starting to get User data...');
+        $users = CognitoUserHelper::listCognitoUsersWithUppercasedEmails();
+
+        if (count($users) === 0) {
+            return $this->info('No users found with uppercased emails.');
+        }
+
+        foreach ($users as $num => $user) {
+            $email = $user['email'];
+
+            // Return response to console
+            $number = ($num+1);
+            $response .= $this->line($number.'. User ['.$user['id'].'] ... "'.$email.'"');
+        }
+
+        $response .= $this->info('Done finding users.');
+
+        return $response;
+    }
+}
