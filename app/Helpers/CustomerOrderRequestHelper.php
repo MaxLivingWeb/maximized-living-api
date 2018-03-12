1fa26d06919e3d7371a0b0970ad70c6947d5ee0c
Jessica Mundt <j.mundt@arcane.ws>
2018-03-12 12:55:10 -0400

    + Updated retail reporting, so there is 2 separate reporting options: "Retail Customers" and "POS Retail" + Tidied reporting controllers, so global DateRequestHelper is being used + Tidied reporting controllers, so global CustomerOrderRequestHelper is being used

---
+++ app/Helpers/CustomerOrderRequestHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Helpers\DateRequestHelper;
+use App\Helpers\ShopifyHelper;
+use Illuminate\Http\Request;
+
+class CustomerOrderRequestHelper
+{
+    /**
+     * Get All Shopify Orders - based on provided request paramters
+     */
+    public static function getAllOrders(Request $request)
+    {
+        $shopify = new ShopifyHelper();
+
+        $dateObject = DateRequestHelper::getDateObject($request);
+        $startDate = $dateObject->startDate;
+        $endDate = $dateObject->endDate;
+
+        return $shopify->getAllOrders($startDate, $endDate, request()->input('status'));
+    }
+}
